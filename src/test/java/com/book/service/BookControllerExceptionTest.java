237873edd60b667b7a09df92c839fa7c69a65866
Diogo Santos <diogoffs@outlook.com>
2020-11-27 12:07:26 +0000

    Adding active profile for tests

--- src/test/java/com/book/service/BookControllerExceptionTest.java
+++ src/test/java/com/book/service/BookControllerExceptionTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -19,6 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 public class BookControllerExceptionTest {
 	@Autowired
